581ffbb76429c5081f79ace3e10f820a01b87bbe
Caio Teixeira <caiovmteixeira@outlook.com>
2021-03-13 16:49:28 -0300

    Play around with table layout :)

--- src/templates/companyPage.js
+++ src/templates/companyPage.js
@@ -7,17 +7,17 @@ const AccountTable = (props) => {
 
   return (
     <div>
-      <p>{description}</p>
-      <table>
-        <tr>
-          <th>Ano</th>
-          <th>Valor</th>
+      <p class="text-lg text-center font-bold m-5">{description}</p>
+      <table class="rounded-t-lg m-5 w-5/6 mx-auto bg-gray-200 text-gray-800">
+        <tr class="text-left border-b-2 border-gray-300">
+          <th class="px-4 py-3">Ano</th>
+          <th class="px-4 py-3">Valor</th>
         </tr>
         {
           values.map(value => 
-            <tr key={value[0]}>
-              <td>{value[0]}</td>
-              <td>{value[1].value}</td>
+            <tr key={value[0]} class="bg-gray-100 border-b border-gray-200">
+              <td class="px-4 py-3">{value[0]}</td>
+              <td class="px-4 py-3">{value[1].value}</td>
             </tr>
           )
         }
@@ -56,8 +56,8 @@ export default function CompanyPage({data}) {
 
   return (
     <div>
-      <h1>{companyData.name}</h1>
-      <p>{companyData.cnpj}</p>
+      <h1 class="text-lg text-center font-bold text-indigo-700">{companyData.name}</h1>
+      <p class="text-center text-indigo-600">{companyData.cnpj}</p>
       <AccountList dfp={dfp}/>
     </div>
   )
