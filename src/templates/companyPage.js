203de84ee7b21cb086724961521170f9a26b2284
Caio Teixeira <caiovmteixeira@outlook.com>
2021-03-12 22:57:05 -0300

    fetch dfp data

--- src/templates/companyPage.js
+++ src/templates/companyPage.js
@@ -1,14 +1,25 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import { graphql } from "gatsby"
 
 
 export default function CompanyPage({data}) {
   const companyData = data.dataJson
+  const [dfp, setDfp] = useState({})
+
+  useEffect(() => {
+    fetch(`../data/${companyData.id}.json`)
+    .then(response => response.json())
+    .then(json => {
+      console.log(json)
+      setDfp(json)
+    })
+  }, [])
 
   return (
     <div>
       <h1>{companyData.name}</h1>
       <p>{companyData.cnpj}</p>
+      <p>{JSON.stringify(dfp)}</p>
     </div>
   )
 }
