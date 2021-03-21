f8963d66489896242383077973265fd2083aa32e
Caio Teixeira <caiovmteixeira@outlook.com>
2021-03-21 20:11:42 -0300

    Create fca table

--- data-extraction/extract.js
+++ data-extraction/extract.js
@@ -1,6 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const sqlite = require('sqlite');
-const fs = require('fs')
+const fs = require('fs');
+const neatCsv = require('neat-csv');
 
 async function getAccount(db, cia_id, account_id) {
   let dfp_query =
@@ -40,49 +41,96 @@ async function getAccount(db, cia_id, account_id) {
     return account;
 }
 
-async function extractDFP(db, cia_id) {
-  let dfp_query =
-    `SELECT YEAR year,
-                  VERSAO version,
-                  CD_CONTA accountCode,
-                  DS_CONTA accountDescription,
-                  VL_CONTA value
-            FROM dfp
-            WHERE ID_CIA == ?
-            ORDER BY CD_CONTA`;
-  let dfp = {}
+async function prepare_fca_table(db) {
+  let drop = `DROP TABLE IF EXISTS "fca_mobiliario"`;
+  await db.exec(drop);
 
-  await db.each(dfp_query, [cia_id], (err, row) => {
-    if (err) {
-      throw err;
-    }
+  let create = 
+    `CREATE TABLE "fca_mobiliario" (
+        "CNPJ_Companhia"	TEXT,
+        "Data_Referencia"	TEXT,
+        "Versao"	INTEGER,
+        "ID_Documento"	INTEGER,
+        "Valor_Mobiliario"	TEXT,
+        "Sigla_Classe_Acao_Preferencial"	TEXT,
+        "Classe_Acao_Preferencial"	TEXT,
+        "Codigo_Negociacao"	TEXT,
+        "Composicao_BDR_Unit"	TEXT,
+        "Mercado"	TEXT,
+        "Sigla_Entidade_Administradora"	TEXT,
+        "Entidade_Administradora"	TEXT,
+        "Data_Inicio_Negociacao"	TEXT,
+        "Data_Fim_Negociacao"	TEXT,
+        "Segmento"	TEXT,
+        "Data_Inicio_Listagem"	TEXT,
+        "Data_Fim_Listagem"	TEXT
+      )`;
+  await db.exec(create);
+}
 
-    if (dfp[row.accountCode] == undefined) {
-      let entry = {};
-      entry.accountCode = row.accountCode;
-      entry.accountDescripton = row.accountDescription;
-      entry.data = {};
-      dfp[row.accountCode] = entry;
-    }
+async function load_fca(db) {
+  console.log("Begin to load FCA");
 
-    if (dfp[row.accountCode].data[row.year] == undefined) {
-      dfp[row.accountCode].data[row.year] = {};
-    }
+  await prepare_fca_table(db);
 
-    let yearEntry = dfp[row.accountCode].data[row.year];
-    if (yearEntry.version > row.version) {
-      return;
-    }
+  let stream = fs.readFileSync('.data/fca_cia_aberta_valor_mobiliario_2020.csv');
 
-    yearEntry.version = row.version;
-    yearEntry.value = row.value;
-  });
+  const csv = await neatCsv(stream, {separator: ';'});
 
-  return dfp;
-};
+  let array = csv.map(entry => Object.entries(entry).map(value => value[1]));
+  console.log(array);
+
+  for (let index = 0; index < array.length; index++) {
+    const values = array[index];
+    
+    db.run(`INSERT INTO "fca_mobiliario" (
+      "CNPJ_Companhia",
+      "Data_Referencia",
+      "Versao",
+      "ID_Documento",
+      "Valor_Mobiliario",
+      "Sigla_Classe_Acao_Preferencial",
+      "Classe_Acao_Preferencial",
+      "Codigo_Negociacao",
+      "Composicao_BDR_Unit",
+      "Mercado",
+      "Sigla_Entidade_Administradora",
+      "Entidade_Administradora",
+      "Data_Inicio_Negociacao",
+      "Data_Fim_Negociacao",
+      "Segmento",
+      "Data_Inicio_Listagem",
+      "Data_Fim_Listagem"
+    ) VALUES (
+      ?,
+      ?,
+      ?,
+      ?,
+      ?,
+      ?,
+      ?,
+      ?,
+      ?,
+      ?,
+      ?,
+      ?,
+      ?,
+      ?,
+      ?,
+      ?,
+      ?
+    )`, values);
+
+
+  }
+
+  console.log("loaded FCA :) ");
+}
 
 async function main() {
   const db = await sqlite.open({ filename: '.data/rapina.db', driver: sqlite3.Database });
+
+  await load_fca(db);
 
   let sql = `SELECT ID id,
                   CNPJ cnpj,
