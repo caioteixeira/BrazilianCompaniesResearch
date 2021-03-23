5efc79ca711fe6aab3eb0b7fbe25b1c99f65e574
Caio Teixeira <caiovmteixeira@outlook.com>
2021-03-23 20:50:09 -0300

    Add simples styling to initial page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,24 @@ import { Link, graphql } from "gatsby"
 
 export default function Home( {data} ) {
   return (
-    <div>
-      <h1>Hello world!</h1>
-      {data.allDataJson.edges.map(({ node }) => (
-        <div key={node.id}>
-          <Link to={node.shortTicker}>
-            <button>
-              {node.shortTicker} - {node.name}
-            </button>
-          </Link>
+    <div class="bg-blue-200 p-6">
+      <div class="bg-white rounded-xl shadow-md mx-auto container p-6">
+        <h1 class="text-center text-2xl">Empresas listadas na B3</h1>
+        <div class="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 py-4">
+          {data.allDataJson.edges.map(({ node }) => (
+            <div key={node.id} class="p-2">
+              <Link to={node.shortTicker}>
+                <p class="text-lg">
+                  {node.shortTicker}
+                </p>
+                <p class="text-sm text-gray-400">
+                  {node.name}
+                </p>
+              </Link>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </div>
   )
 }
