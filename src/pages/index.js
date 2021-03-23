1a4f8d41216ba448ddad3e666ae63e4898b37d4d
Caio Teixeira <caiovmteixeira@outlook.com>
2021-03-23 20:14:36 -0300

    Add all links to initial page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,32 @@
 import React from "react"
+import { Link, graphql } from "gatsby"
 
-export default function Home() {
-  return <div>Hello world!</div>
+
+export default function Home( {data} ) {
+  return (
+    <div>
+      <h1>Hello world!</h1>
+      {data.allDataJson.edges.map(({ node }) => (
+        <div key={node.id}>
+          <Link to={node.shortTicker}>
+            <button>
+              {node.shortTicker} - {node.name}
+            </button>
+          </Link>
+        </div>
+      ))}
+    </div>
+  )
 }
+
+export const query = graphql`
+  query {
+      allDataJson {
+        edges {
+          node {
+            name
+            shortTicker
+          }
+        }
+      }
+    }`
