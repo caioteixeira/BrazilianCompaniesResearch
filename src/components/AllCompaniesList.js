b962e4585dcb67c75a4b79e1fab6f03c8abbc5d6
Caio Teixeira <caiovmteixeira@outlook.com>
2021-03-27 14:52:38 -0300

    Add pagination

--- src/components/AllCompaniesList.js
+++ src/components/AllCompaniesList.js
@@ -1,8 +1,21 @@
-import React from "react"
+import React, {useState} from "react"
 import CompanyCell from "./CompanyCell"
+import ReactPaginate from 'react-paginate'
 
 const AllCompaniesList = (props) => {
-  const allCompanies = props.allCompanies.slice(0,32);
+  const [currentPage, setCurrentPage] = useState(0);
+  const companiesPerPage = 12;
+
+  const pageCount = props.allCompanies.length / companiesPerPage;
+  const floor = companiesPerPage * currentPage;
+  const ceil = Math.ceil(floor + companiesPerPage, props.allCompanies.length);
+
+  const allCompanies = props.allCompanies.slice(floor,ceil);
+
+  const onPageClick = (data) => {
+    console.log(data);
+    setCurrentPage(data.selected);
+  }
 
   return (
     <>
@@ -12,6 +25,22 @@ const AllCompaniesList = (props) => {
           <CompanyCell key={node.id} shortTicker={node.shortTicker} name={node.name} />
         ))}
       </div>
+      
+      <ReactPaginate
+          previousLabel={'<'}
+          nextClassName={'p-2 mx-1 text-blue-800 shadow hover:bg-blue-300 rounded-md text-xl h-10 w-10 text-center'}
+          previousClassName={'p-2 mx-1 text-blue-800 shadow hover:bg-blue-300 rounded-md text-xl h-10 w-10 text-center'}
+          nextLabel={'>'}
+          breakLabel={'...'}
+          breakClassName={'p-2'}
+          pageCount={pageCount}
+          marginPagesDisplayed={1}
+          pageRangeDisplayed={2}
+          onPageChange={onPageClick}
+          pageClassName={'p-2 mx-1 shadow hover:bg-blue-200 rounded-md text-xl h-10 w-10 text-center'}
+          containerClassName={'mx-auto flex justify-center'}
+          activeClassName={'active bg-blue-200 bg-opacity-75 inline-block font-semibold'}
+        />
     </>
   )
 }
