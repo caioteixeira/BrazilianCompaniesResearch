3a9daf904ee3bc3bbe73894a25a8c6a6a4ee7ac8
Caio Teixeira <caiovmteixeira@outlook.com>
2021-03-27 20:28:35 -0300

    Remove pagination, use a load more button instead

--- src/components/AllCompaniesList.js
+++ src/components/AllCompaniesList.js
@@ -1,20 +1,16 @@
 import React, {useState} from "react"
 import CompanyCell from "./CompanyCell"
-import ReactPaginate from 'react-paginate'
 
 const AllCompaniesList = (props) => {
-  const [currentPage, setCurrentPage] = useState(0)
+  const [currentPage, setCurrentPage] = useState(1)
   const companiesPerPage = 12
 
-  const pageCount = props.allCompanies.length / companiesPerPage
-  const floor = companiesPerPage * currentPage
-  const ceil = Math.ceil(floor + companiesPerPage, props.allCompanies.length)
+  const ceil = Math.ceil(companiesPerPage * currentPage, props.allCompanies.length)
 
-  const allCompanies = props.allCompanies.slice(floor,ceil)
+  const allCompanies = props.allCompanies.slice(0,ceil)
 
-  const onPageClick = (data) => {
-    console.log(data)
-    setCurrentPage(data.selected)
+  const onLoadMoreClick = (data) => {
+    setCurrentPage(currentPage + 1)
   }
 
   return (
@@ -25,22 +21,12 @@ const AllCompaniesList = (props) => {
           <CompanyCell key={node.id} shortTicker={node.shortTicker} name={node.name} />
         ))}
       </div>
-      
-      <ReactPaginate
-          previousLabel={'<'}
-          nextClassName={'p-2 mx-1 text-blue-800 shadow hover:bg-blue-300 rounded-md text-xl h-10 w-10 text-center'}
-          previousClassName={'p-2 mx-1 text-blue-800 shadow hover:bg-blue-300 rounded-md text-xl h-10 w-10 text-center'}
-          nextLabel={'>'}
-          breakLabel={'...'}
-          breakClassName={'p-2'}
-          pageCount={pageCount}
-          marginPagesDisplayed={1}
-          pageRangeDisplayed={2}
-          onPageChange={onPageClick}
-          pageClassName={'p-2 mx-1 shadow hover:bg-blue-200 rounded-md text-xl h-10 w-10 text-center'}
-          containerClassName={'mx-auto flex justify-center'}
-          activeClassName={'active bg-blue-200 bg-opacity-75 inline-block font-semibold'}
-        />
+      <div className="mx-auto flex justify-center">
+        <button className="text-blue-700 font-semibold hover:text-blue-900 hover:underline py-2 px-4 rounded" 
+          onClick={onLoadMoreClick}>
+          Ver mais empresas
+        </button>
+      </div>
     </>
   )
 }
