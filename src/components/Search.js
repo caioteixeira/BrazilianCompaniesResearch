0cf69aa4dc8f56cfbacc9b366f4c3dac21f300b9
Caio Teixeira <caiovmteixeira@outlook.com>
2021-04-23 23:00:33 -0300

    Extract Search results into separate components

--- src/components/Search.js
+++ src/components/Search.js
@@ -8,8 +8,29 @@ import { Box, Input, Stack, LinkBox, LinkOverlay, Heading,
 import Logo from "./Logo"
 
 
-const ItemsList = (props) => {
+const ItemsList = ({children}) => {
+  return (
+    <Stack>
+      {children}
+    </Stack>
+  )
+}
 
+const Item = ({highlighted, node}) => {
+  return (
+    <LinkBox key={node.id} 
+        bgColor={highlighted ? "gray.100" : "white"}>
+      <Stack direction="row" alignContent="center">
+        <Logo ticker={node.ticker} name={`${node.ticker} logo`} size={10} 
+            ></Logo>  
+        <Heading size="md" my="auto">
+          <Link to={`../${node.ticker}`}>
+            <LinkOverlay>{node.name}</LinkOverlay>
+          </Link>
+        </Heading > 
+      </Stack>
+    </LinkBox>
+  )
 }
 
 const SearchBox = (props) => {
@@ -32,8 +53,6 @@ const SearchBox = (props) => {
   const itemToString = item => (item ? item.name : '')
   const {
     isOpen,
-    getToggleButtonProps,
-    getLabelProps,
     getMenuProps,
     highlightedIndex,
     getItemProps,
@@ -60,26 +79,18 @@ const SearchBox = (props) => {
         <Input ref={initialRef} placeholder="Pesquisar empresas" 
             bgColor="white" shadow="lg" {...getInputProps()} />
       </Box>
-      <Stack {...getMenuProps()}>
+      <ItemsList {...getMenuProps()}>
         {isOpen && results.map(( node, index ) => (
-            <LinkBox key={node.id} 
-                bgColor={index === highlightedIndex ? "gray.100" : "white"}
-                {...getItemProps({
-                  node,
-                  index,
-                })}>
-              <Stack direction="row" alignContent="center">
-                <Logo ticker={node.ticker} name={`${node.ticker} logo`} size={10} 
-                    ></Logo>  
-                <Heading size="md" my="auto">
-                  <Link to={`../${node.ticker}`}>
-                    <LinkOverlay>{node.name}</LinkOverlay>
-                  </Link>
-                </Heading > 
-              </Stack>
-            </LinkBox>
-          ))}
-      </Stack>
+          <Item key={node.id} 
+              highlighted = {index === highlightedIndex}
+              node = {node}
+              {...getItemProps({
+                node,
+                index,
+              })}>
+          </Item>
+        ))}
+      </ItemsList>
     </>
   )
 }
